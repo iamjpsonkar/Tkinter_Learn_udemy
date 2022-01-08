4310f498bf6633a71aea9a1a27e0f2a5f8a44af9
Jay Prakash Sonkar <iamjpsonkar@gmail.com>
2022-01-08 12:08:37 +0530

    Learned Entry

---
+++ 04_entry.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import ttk
+
+def greetme():
+    print("Hello {}!!!".format(name.get() or "World"))
+
+root=tk.Tk()
+name=tk.StringVar()
+name_l=ttk.Label(root,text="Name: ")
+name_l.pack(side="left")
+name_e=ttk.Entry(root,textvariable=name, width=15)
+name_e.pack(side="left")
+greet_b=ttk.Button(root,text="Greet",command=greetme)
+greet_b.pack(side="left")
+quit_b=ttk.Button(root,text="Quit",command=root.destroy)
+quit_b.pack(side="right")
+
+
+root.mainloop()
